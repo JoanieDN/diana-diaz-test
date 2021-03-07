0cb727dda3cb985eade260e0e34b0be54ee00615
Diana Diaz Navarrete <dianajdiaz.122@gmail.com>
2021-03-07 14:45:08 -0600

    prueba 2

--- src/app/modules/person/person.module.ts
+++ src/app/modules/person/person.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonListComponent } from './person-list/person-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PersonListComponent],
   imports: [
     CommonModule
   ]
