9deedd12da7837c578b0240a31aabaa04f8ea04b
Diana Diaz Navarrete <dianajdiaz.122@gmail.com>
2021-03-07 16:12:18 -0600

    Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PersonComponent } from './module/person/person.component';
+import { PersonListComponent } from './person-list/person-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PersonComponent ],
+  declarations: [ AppComponent, HelloComponent, PersonComponent, PersonListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
